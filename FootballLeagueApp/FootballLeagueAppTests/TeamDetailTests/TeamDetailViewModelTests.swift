07b96f5c32a5e99ee44baca8044c7d47c1463d8f
David Castillo Mendoza <davidc.mendoza31@gmail.com>
2025-01-14 15:32:43 -0600

    Adds TeamDetailViewModelTests file

---
+++ FootballLeagueApp/FootballLeagueAppTests/TeamDetailTests/TeamDetailViewModelTests.swift
@@ -0,0 +1,20 @@
+//
+//  TeamDetailViewModelTests.swift
+//  FootballLeagueAppTests
+//
+//  Created by David Ali on 14/01/25.
+//
+
+import XCTest
+
+final class TeamDetailViewModelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
