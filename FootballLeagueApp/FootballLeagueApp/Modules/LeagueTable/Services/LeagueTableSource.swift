85b86363615d6e8062d7563a166729274e9e8ce4
David Castillo Mendoza <davidc.mendoza31@gmail.com>
2025-01-14 10:24:33 -0600

    Adds logic to fetch league table data from api

--- FootballLeagueApp/FootballLeagueApp/Modules/LeagueTable/Services/LeagueTableSource.swift
+++ FootballLeagueApp/FootballLeagueApp/Modules/LeagueTable/Services/LeagueTableSource.swift
@@ -13,8 +13,29 @@ protocol LeagueTableSource {
 
 final class LeagueTableApiSource: LeagueTableSource {
     
+    let baseURL = URL(string: "https://www.thesportsdb.com/api/v1/json/3/lookuptable.php?l=4328&s=2020-2021")
+    
     func fetchLeagueTable() async -> [LeagueTableItem] {
-        return []
+        let components = URLComponents(url: baseURL!, resolvingAgainstBaseURL: true)
+        let request = URLRequest(url: components!.url!)
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            guard let urlResponse = response as? HTTPURLResponse,
+                  (200...299).contains(urlResponse.statusCode) else {
+                return []
+            }
+            
+            let decoder = JSONDecoder()
+            let decodedData = try decoder.decode(LeagueTableResult.self, from: data)
+            
+            let items = decodedData.results.map { LeagueTableItem(resultItem: $0) }
+            return items
+        } catch let error {
+            debugPrint(error)
+            return []
+        }
     }
     
 }
