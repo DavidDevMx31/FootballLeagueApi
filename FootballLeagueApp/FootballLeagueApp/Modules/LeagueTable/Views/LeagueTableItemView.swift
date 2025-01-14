30f827fd19d42af864618a1d467baa32ea776503
David Castillo Mendoza <davidc.mendoza31@gmail.com>
2025-01-14 10:41:34 -0600

    Adds view for league table items

--- FootballLeagueApp/FootballLeagueApp/Modules/LeagueTable/Views/LeagueTableItemView.swift
+++ FootballLeagueApp/FootballLeagueApp/Modules/LeagueTable/Views/LeagueTableItemView.swift
@@ -8,13 +8,74 @@
 import SwiftUI
 
 struct LeagueTableItemView: View {
+    let rank: String
+    let imageURL: URL?
+    let teamName: String
+    let win: Int
+    let draw: Int
+    let loss: Int
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .center, spacing: 8) {
+            Text(rank)
+                .minimumScaleFactor(0.8)
+                .lineLimit(1)
+                .bold()
+                .frame(width: 20)
+            
+            AsyncImage(url: imageURL) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+            } placeholder: {
+                Color.gray
+            }
+            .frame(width: 36, height: 36)
+
+                
+            Text(teamName)
+                .minimumScaleFactor(0.8)
+                .lineLimit(2)
+                .multilineTextAlignment(.leading)
+                .frame(alignment: .leading)
+            
+            Spacer()
+            
+            Text("\(win)")
+                .lineLimit(1)
+                .italic()
+                .frame(width: 28)
+            
+            Text("\(draw)")
+                .lineLimit(1)
+                .italic()
+                .frame(width: 28)
+            
+            Text("\(loss)")
+                .lineLimit(1)
+                .italic()
+                .frame(width: 28)
+            
+        }
     }
+    
+}
+
+extension LeagueTableItemView {
+    
+    init(item: LeagueTableItem) {
+        self.rank = "\(item.rank)"
+        self.imageURL = URL(string: item.badgeUrl)
+        self.teamName = item.teamName
+        self.win = item.wins
+        self.draw = item.draws
+        self.loss = item.losses
+    }
+    
 }
 
 struct LeagueTableItemView_Previews: PreviewProvider {
     static var previews: some View {
-        LeagueTableItemView()
+        LeagueTableItemView(item: LeagueTableItem.example)
     }
 }
