9dadc70f1e8cf727793451b46fa65a70295c35cd
David Castillo Mendoza <davidc.mendoza31@gmail.com>
2025-01-14 14:05:00 -0600

    Fetches and data from source in viewModel

--- FootballLeagueApp/FootballLeagueApp/Modules/TeamDetail/TeamDetailViewModel.swift
+++ FootballLeagueApp/FootballLeagueApp/Modules/TeamDetail/TeamDetailViewModel.swift
@@ -9,9 +9,21 @@ import Foundation
 
 final class TeamDetailViewModel: ObservableObject {
     @Published private(set) var teamDetail: TeamDetail?
+    private let source: TeamDetailSource
+    
+    init(source: TeamDetailSource = TeamDetailApiSource()) {
+        self.source = source
+        self.teamDetail = TeamDetail.example
+    }
     
     @MainActor
     func fetchDetail(for teamName: String) async {
-        teamDetail = TeamDetail.example
+        let results = await source.fetchTeamDetailBy(name: teamName)
+        
+        if results.count == 1 {
+            teamDetail = results[0]
+        } else {
+            teamDetail = nil
+        }
     }
 }
