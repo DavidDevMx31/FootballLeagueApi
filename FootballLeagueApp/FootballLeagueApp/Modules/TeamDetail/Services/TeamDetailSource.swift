6bc5c9285ec3b4fe7c32490a71359b036389a1ff
David Castillo Mendoza <davidc.mendoza31@gmail.com>
2025-01-14 12:38:24 -0600

    Fetches and decodes team detail data

--- FootballLeagueApp/FootballLeagueApp/Modules/TeamDetail/Services/TeamDetailSource.swift
+++ FootballLeagueApp/FootballLeagueApp/Modules/TeamDetail/Services/TeamDetailSource.swift
@@ -10,3 +10,35 @@ import Foundation
 protocol TeamDetailSource {
     func fetchTeamDetailBy(name: String) async -> [TeamDetail]
 }
+
+final class TeamDetailApiSource: TeamDetailSource {
+    
+    let baseURL = URL(string: "https://www.thesportsdb.com/api/v1/json/3/searchteams.php")
+    
+    func fetchTeamDetailBy(name: String) async -> [TeamDetail] {
+        var components = URLComponents(url: baseURL!, resolvingAgainstBaseURL: true)
+        components?.queryItems = [
+            URLQueryItem(name: "t", value: name)
+        ]
+        let request = URLRequest(url: components!.url!)
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            guard let urlResponse = response as? HTTPURLResponse,
+                  (200...299).contains(urlResponse.statusCode) else {
+                return []
+            }
+            
+            let decoder = JSONDecoder()
+            let decodedData = try decoder.decode(SearchTeamsResponse.self, from: data)
+            
+            let details = decodedData.teams.map { TeamDetail(response: $0)}
+            return details
+            
+        } catch let error {
+            debugPrint(error)
+            return []
+        }
+    }
+}
