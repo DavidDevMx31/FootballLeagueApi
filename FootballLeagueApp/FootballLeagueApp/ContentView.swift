83199aa25fa9189a83ed9370beab87fa7d473b22
David Castillo Mendoza <davidc.mendoza31@gmail.com>
2025-01-14 09:38:32 -0600

    Initial commit

---
+++ FootballLeagueApp/FootballLeagueApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  FootballLeagueApp
+//
+//  Created by David Ali on 14/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FootballLeagueApp/FootballLeagueApp/FootballLeagueAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballLeagueAppApp.swift
+//  FootballLeagueApp
+//
+//  Created by David Ali on 14/01/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballLeagueAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
