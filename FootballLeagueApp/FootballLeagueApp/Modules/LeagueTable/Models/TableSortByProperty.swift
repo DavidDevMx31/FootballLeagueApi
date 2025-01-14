ffa13a2c3bb8dedd277040ae53e9e1e68366e327
David Castillo Mendoza <davidc.mendoza31@gmail.com>
2025-01-14 10:48:28 -0600

    Adds enum with property options to sort league data

---
+++ FootballLeagueApp/FootballLeagueApp/Modules/LeagueTable/Models/TableSortByProperty.swift
@@ -0,0 +1,15 @@
+//
+//  TableSortByProperty.swift
+//  FootballLeagueApp
+//
+//  Created by David Ali on 14/01/25.
+//
+
+import Foundation
+
+enum TableSortByProperty: String, CaseIterable {
+    case rank = "Rank"
+    case win = "Win"
+    case draw = "Draw"
+    case loss = "Loss"
+}
